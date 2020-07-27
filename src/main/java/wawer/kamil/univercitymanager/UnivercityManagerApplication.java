33588e93343d18a48e5e51c3da4dcbfa43ee63b0
Kamil Wawer <kamil.wawer@pollub.edu.pl>
2020-07-27 13:13:59 +0200

    Initial commit

---
+++ src/main/java/wawer/kamil/univercitymanager/UnivercityManagerApplication.java
@@ -0,0 +1,13 @@
+package wawer.kamil.univercitymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnivercityManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnivercityManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/wawer/kamil/univercitymanager/UnivercityManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package wawer.kamil.univercitymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnivercityManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
